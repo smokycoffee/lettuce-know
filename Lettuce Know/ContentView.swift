a6a81df5d127693195f41ff9f6a442a273db5b70
Tsenguun <Tsenguun.batbold12@gmail.com>
2023-02-20 15:00:40 +0100

    initialise project

---
+++ Lettuce Know/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lettuce Know
+//
+//  Created by Tsenguun on 16/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lettuce Know/Lettuce_KnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lettuce_KnowApp.swift
+//  Lettuce Know
+//
+//  Created by Tsenguun on 16/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Lettuce_KnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
